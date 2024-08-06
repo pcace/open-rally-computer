99038ddd9e43943f6c5188c363150f1a1ada2a90
pcace <{ID}+{username}@users.noreply.github.com>
2024-08-06 20:47:32 +0200

    fix: :bug: fix memory overflow when showing too many items in html list

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -13,6 +13,7 @@ const char *ssid = "orc";
 const char *password = "12345678";
 
 AsyncWebServer server(80);
+const int filesPerPage = 10;
 
 void initializeWifi()
 {
@@ -27,36 +28,6 @@ void initializeWifi()
     else
     {
         Serial.println("Access Point Failed to Start");
-    }
-}
-
-void listFilesInDir(File dir, String &html, String path)
-{
-    while (true)
-    {
-        File entry = dir.openNextFile();
-        if (!entry)
-        {
-            // No more files
-            break;
-        }
-        // Ignore hidden files and directories (names starting with '.')
-        if (entry.name()[0] == '.')
-        {
-            entry.close();
-            continue;
-        }
-        String entryPath = path + entry.name();
-        if (entry.isDirectory())
-        {
-            listFilesInDir(entry, html, entryPath + "/");
-        }
-        else
-        {
-            // Create a link for each file
-            html += "<a href=\"" + entryPath + "\" download>" + entry.name() + "</a> <form action=\"/delete\" method=\"post\" style=\"display:inline;\" onsubmit=\"return confirm('Really delete?');\"><button name=\"file\" value=\"" + entryPath + "\">Delete</button></form><br/>";
-        }
-        entry.close();
     }
 }
 
@@ -80,7 +51,7 @@ String generateStatusHTML()
     html += "<tr><td>state.currentLatitude</td><td>" + String(state.currentLatitude) + "</td></tr>";
     html += "<tr><td>state.currentLongitude</td><td>" + String(state.currentLongitude) + "</td></tr>";
     html += "<tr><td>state.gpsSatellites</td><td>" + String(state.gpsSatellites) + "</td></tr>";
-    html += "<tr><td>state.currentAltitude</td><td>" + String(state.currentAltitude,2) + "</td></tr>";
+    html += "<tr><td>state.currentAltitude</td><td>" + String(state.currentAltitude, 2) + "</td></tr>";
     html += "<tr><td>state.timeHours</td><td>" + String(state.timeHours) + "</td></tr>";
     html += "<tr><td>state.timeMinutes</td><td>" + String(state.timeMinutes) + "</td></tr>";
     html += "<tr><td>state.timeSeconds</td><td>" + String(state.timeSeconds) + "</td></tr>";
@@ -99,7 +70,7 @@ String generateStatusHTML()
     html += "<tr><td>state.updatingFirmware</td><td>" + String(state.updatingFirmware) + "</td></tr>";
     html += "<tr><td>state.currentTrackFile</td><td>" + String(state.currentTrackFile.c_str()) + "</td></tr>";
     html += "<tr><td>state.currentGPXFile</td><td>" + String(state.currentGPXFile.c_str()) + "</td></tr>";
-  
+
     html += "<tr><td>memory.config.avgAccumulator</td><td>" + String(memory.config.avgAccumulator) + "</td></tr>";
     html += "<tr><td>memory.config.avgSamples</td><td>" + String(memory.config.avgSamples) + "</td></tr>";
     html += "<tr><td>memory.config.language</td><td>" + String(memory.config.language) + "</td></tr>";
@@ -117,12 +88,42 @@ String generateStatusHTML()
     html += "<tr><td>memory.config.memoryInitialized</td><td>" + String(memory.config.memoryInitialized) + "</td></tr>";
     html += "<tr><td>memory.config.saveInterval</td><td>" + String(memory.config.saveInterval) + "</td></tr>";
 
-
     html += "</table></body></html>";
     return html;
 }
 
-String generateFileListHTML()
+void listFilesInDir(File dir, String &html, String path, int start, int count)
+{
+    int currentFile = 0;
+    int end = start + count;
+    while (currentFile < end)
+    {
+        File entry = dir.openNextFile();
+        if (!entry)
+        {
+            break; // No more files
+        }
+        if (entry.name()[0] == '.') // Ignore hidden files
+        {
+            entry.close();
+            continue;
+        }
+        if (currentFile >= start)
+        {
+            String entryPath = path + entry.name();
+            if (!entry.isDirectory())
+            {
+                html += "<a href=\"" + entryPath + "\" download>" + entry.name() + "</a> ";
+                html += "<form action=\"/delete\" method=\"post\" style=\"display:inline;\" onsubmit=\"return confirm('Really delete?');\">";
+                html += "<button name=\"file\" value=\"" + entryPath + "\">Delete</button></form><br/>";
+            }
+        }
+        entry.close();
+        currentFile++;
+    }
+}
+
+String generateFileListHTML(int page)
 {
     String html = "<!DOCTYPE html><html><body><h3>Tracks</h3>";
     File root = SD.open("/");
@@ -136,24 +137,38 @@ String generateFileListHTML()
     }
     else
     {
-        listFilesInDir(root, html, "/");
+        listFilesInDir(root, html, "/", page * filesPerPage, filesPerPage);
     }
     root.close();
+
+    // Pagination controls
+    html += "<br/>";
+    if (page > 0)
+    {
+        html += "<a href=\"/?page=" + String(page - 1) + "\">Previous</a> ";
+    }
+    html += "<a href=\"/?page=" + String(page + 1) + "\">Next</a>";
+
     html += "</body></html>";
     return html;
 }
 
 void initializeWebserver()
 {
-    // Web server routes
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
               {
-        String htmlContent = generateFileListHTML();
-        request->send(200, "text/html", htmlContent); });
+                  int page = 0;
+                  if (request->hasParam("page"))
+                  {
+                      page = request->getParam("page")->value().toInt();
+                  }
+                  String htmlContent = generateFileListHTML(page);
+                  request->send(200, "text/html", htmlContent); });
+
     server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request)
               {
-        String htmlContent = generateStatusHTML();
-        request->send(200, "text/html", htmlContent); });
+                  String htmlContent = generateStatusHTML();
+                  request->send(200, "text/html", htmlContent); });
 
     // Serve files from the SD card
     server.serveStatic("/", SD, "/");
@@ -161,16 +176,20 @@ void initializeWebserver()
     // Route to handle file deletion
     server.on("/delete", HTTP_POST, [](AsyncWebServerRequest *request)
               {
-        if (request->hasParam("file", true)) {
-            AsyncWebParameter* p = request->getParam("file", true);
-            String filePath = p->value();
-            if (SD.remove(filePath.c_str())) {
-                Serial.println("File Deleted");
-            } else {
-                Serial.println("Delete Failed");
-            }
-        }
-        request->redirect("/"); });
+                  if (request->hasParam("file", true))
+                  {
+                      AsyncWebParameter *p = request->getParam("file", true);
+                      String filePath = p->value();
+                      if (SD.remove(filePath.c_str()))
+                      {
+                          Serial.println("File Deleted");
+                      }
+                      else
+                      {
+                          Serial.println("Delete Failed");
+                      }
+                  }
+                  request->redirect("/"); });
 
     ElegantOTA.begin(&server); // Start ElegantOTA
 
